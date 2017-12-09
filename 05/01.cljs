b91413014ef4c89935ac894aee566d5c4fc23fa2
i-s-o-g-r-a-m <i-s-o-g-r-a-m@users.noreply.github.com>
2017-12-09 10:27:19 -0500

    05 - part 1 - ClojureScript

---
+++ 05/01.cljs
@@ -0,0 +1,35 @@
+#!/usr/bin/env planck
+
+
+(require '[clojure.string :as s])
+(require '[planck.core :as c])
+
+
+(def input
+  (let [raw (c/slurp "input.txt")]
+    (mapv js/parseInt (s/split-lines raw))))
+
+(defn out-of-bounds
+  [v idx]
+  (or (< idx 0) (> (+ idx 1) (count v))))
+
+(defn jump
+  [v idx]
+  (let
+    [val (nth v idx)]
+    {:v (assoc v idx (+ val 1)) :idx (+ idx val)}))
+
+(defn start
+  ([v] (start v 0 1))
+  ([v idx iteration]
+    (let
+      [{new-idx :idx, new-v :v} (jump v idx)]
+      (if
+        (out-of-bounds v new-idx)
+        iteration
+        (recur new-v new-idx (+ iteration 1))))))
+
+
+(def result (start input))
+(println result)
+(assert (= result 358309))
