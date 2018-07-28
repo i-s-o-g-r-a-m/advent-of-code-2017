a144d9e31a84396d6819ba2a5fd95611fb1b1d0c
i-s-o-g-r-a-m <i-s-o-g-r-a-m@users.noreply.github.com>
2018-07-28 09:56:36 -0400

    10 - part 1 - improvements

--- 11/01.py
+++ 11/01.py
@@ -1,33 +1,88 @@
-input = open('input.txt').read().strip().split(',')
+def traverse_grid(input):
+    directions = {
+        's': [0, 1],
+        'n': [0, -1],
+        'e': [1, 0],
+        'w': [-1, 0],
+    }
 
-coords = [0, 0]
+    coords = [0, 0]
+    for i, move in enumerate(input):
+        odd = abs(coords[0]) % 2
+        if len(move) == 2:
+            d = [x[0] + x[1] for x in zip(directions[move[1]], directions[move[0]])]
+            if not odd and move[0] == 's':
+                d = [d[0], d[1] - 1]
+            if odd and move[0] == 'n':
+                d = [d[0], d[1] + 1]
+        else:
+            d = directions[move]
+        coords = [coords[0] + d[0], coords[1] + d[1]]
+    return coords
 
-directions = {
-    's': [0, 1],
-    'n': [0, -1],
-    'e': [1, 0],
-    'w': [-1, 0],
-}
 
-for i, move in enumerate(input):
-    odd = abs(coords[0]) % 2
-    if len(move) == 2:
-        d = [x[0] + x[1] for x in zip(directions[move[1]], directions[move[0]])]
-        if not odd and move[0] == 's':
-            d = [d[0], d[1] - 1]
-        if odd and move[0] == 'n':
-            d = [d[0], d[1] + 1]
-    else:
-        d = directions[move]
-    coords = [coords[0] + d[0], coords[1] + d[1]]
+def find_direction(from_pos, to_pos):
+    if from_pos == to_pos:
+        raise Exception('...')
+    lat, lng = '', ''
+    if from_pos[1] > to_pos[1]:
+        lat = 'n'
+    elif from_pos[1] < to_pos[1]:
+        lat = 's'
+    if from_pos[0] > to_pos[0]:
+        lng = 'w'
+    elif from_pos[0] < to_pos[0]:
+        lng = 'e'
+    direction = lat + lng
+    assert direction != ''
+    return direction
 
-moves = 0
-while coords != [0, 0]:
-    odd = abs(coords[0]) % 2
-    moves += 1
-    if coords[1] != 0:
-        coords = [coords[0] + 1, coords[1] - 1 if not odd else coords[1]]
-    else:
-        coords = [coords[0] + 1, coords[1]]
-assert moves == 773
-print(moves)
+
+def go_home(position):
+    direction = find_direction(from_pos=position, to_pos=(0, 0))
+
+    moves = 0
+    diagonal_done = False
+
+    while position != [0, 0]:
+        even = not abs(position[0]) % 2
+        moves += 1
+
+        if 0 in position and not diagonal_done:
+            direction = find_direction(from_pos=position, to_pos=(0, 0))
+
+        if len(direction) == 2:
+            if direction == 'ne':
+                position = [position[0] + 1, position[1] - 1 if even else position[1]]
+            elif direction == 'nw':
+                position = [position[0] - 1, position[1] - 1 if even else position[1]]
+            elif direction == 'sw':
+                position = [position[0] - 1, position[1] + 1 if even else position[1]]
+            elif direction == 'se':
+                position = [position[0] + 1, position[1] + 1 if even else position[1]]
+        else:
+            assert len(direction) == 1
+            if direction == 's':
+                position = [position[0], position[1] + 1]
+            elif direction == 'n':
+                position = [position[0], position[1] - 1]
+            elif direction == 'e':
+                position = [position[0] + 1, position[1]]
+            elif direction == 'w':
+                position = [position[0] - 1, position[1]]
+            else:
+                raise Exception('unknown direction {}'.format(direction))
+
+    return moves
+
+
+def main():
+    initial_moves = open('input.txt').read().strip().split(',')
+    position = traverse_grid(initial_moves)
+    return_home_steps = go_home(position)
+    print(return_home_steps)
+    assert return_home_steps == 773
+
+
+if __name__ == '__main__':
+    main()
